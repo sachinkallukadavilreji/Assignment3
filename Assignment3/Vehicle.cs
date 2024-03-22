cb33779cdda0cef2de8b5d04ecc13900f5a94bea
sachinkallukadavilreji <sachinkr690@gmail.com>
2024-03-22 13:47:20 -0400

    Add project files.

---
+++ Assignment3/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Vehicle
+    {
+        private string model {  get; set; }
+        private string manufactureYear { get; set; }
+        private string rentalPrice { get; set; }
+
+        public void displayDetails()
+        {
+            Console.WriteLine("Model name is : " + model);
+            Console.WriteLine("Manufacture year is : " + manufactureYear);
+            Console.WriteLine("Rental price is : " + rentalPrice);
+        }
+
+    }
+}
